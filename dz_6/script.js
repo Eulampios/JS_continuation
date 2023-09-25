f5bd2b760d0229da0538a5330f3b028f76df20d6
citramonus <riecco@mail.ru>
2023-09-25 20:21:39 +0300

    all exercises resolved

---
+++ dz_6/script.js
@@ -0,0 +1,35 @@
+async function fetchData() {
+    try {
+        const response = await fetch('./data.json');
+        if(!response.ok) {
+            throw new Error('данные не получены')
+        }
+
+        const data = await response.json();
+        const content = document.querySelector('.content');
+        data.forEach(({name, description, price, img}) => {
+            
+        const template = `
+        <!-- карточка товара -->
+        <div class="card">
+            <!-- картинка товара -->
+            <div class="image">
+                <img src="${img}" alt="картинка товара">
+            </div>
+            <!-- название, описание, цена -->
+            <div class="text">
+                <p>${name}</p>
+                <p class="description">${description}</p>
+                <p class="price__text">${price}</p>
+            </div>
+        </div>`
+
+            content.insertAdjacentHTML('beforeend', template)
+        });
+
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+fetchData();
